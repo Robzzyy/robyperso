926af7e4478df0991107a7ad575f725a902111d7
Lucas <lucas.robert@lepoles.org>
2022-05-09 16:41:46 +0200

    1er push

---
+++ index.js
@@ -0,0 +1,33 @@
+// let numberOfSeasons = 6;
+// let numberOfEpisodes = 12;
+// let episodeTime = 45;
+// let commercialTime = 5; 
+// let totalShowTime; 
+// totalShowTime =  (episodeTime + commercialTime)  * numberOfEpisodes * numberOfSeasons;
+
+
+// let roby = 2017;
+// let michel = 3;
+// let calcul = roby + michel;
+// document.write(roby + "&nbsp"+ michel);
+// document.write(calcul);
+// document.write('<br>');
+// document.write(calcul);
+// let moi = "7";
+// let calcul2 = roby + michel;
+// document.write(calcul2);
+// console.log(calcul2);
+
+
+
+// class Episode {
+//     constructor (title,duration,hasBeenWatched) {
+//         this.title = title;
+//         this.duration = duration;
+//         this.HasBeenWatched = HasBeenWatched;
+//     }
+// }
+// let Episode = new Episode ("firstEpisode")
+// let Episode = new Episode ("secondEpisode")
+// let Episode = new Episode ("thirdEpisode")
+
